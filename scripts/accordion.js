98e6bcae4f79a077fda28a5a62e9aeed02991738
Quinn <quinn.mohamed12@gmail.com>
2023-08-13 17:57:16 -0400

    Fixed Drop Down Issue

---
+++ scripts/accordion.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionLinks = document.querySelectorAll('.accordion-link');
+    const accordionAnswers = document.querySelectorAll('.answer');
+
+    accordionLinks.forEach((link, index) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            // Toggle the 'active' class on the answer
+            accordionAnswers[index].classList.toggle('active');
+
+            // Toggle the 'add' and 'remove' icons
+            const addIcon = link.querySelector('.add');
+            addIcon.classList.toggle('hidden');
+        });
+    });
+});
